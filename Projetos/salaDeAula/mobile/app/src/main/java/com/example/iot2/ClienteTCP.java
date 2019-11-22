4be23a89ba2ecb9178a43d59c8048a731903f324
victorfernandesraton <43411882+victorfernandesraton@users.noreply.github.com>
2019-11-22 17:46:04 -0300

    aplicativo mobile fix erro de loading

--- Projetos/salaDeAula/mobile/app/src/main/java/com/example/iot2/ClienteTCP.java
+++ Projetos/salaDeAula/mobile/app/src/main/java/com/example/iot2/ClienteTCP.java
@@ -1,12 +1,8 @@
 package com.example.iot2;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-
-
-
 import java.io.*;
 import java.net.*;
 
@@ -14,13 +10,11 @@ import java.net.*;
 public class ClienteTCP {
 
     /**
+     * @description Cliente TCP/UDP para socket arduino anroid
      * @param args the command line arguments
      */
-    public static void main(String[] args){
-    {
-        // TODO code application logic here
+    public static void main(String[] args){ 
         Tela t = new Tela();
         t.setVisible(true);
     }
-
 }
